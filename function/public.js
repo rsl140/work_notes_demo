238fa91de4867bf2f47ea0c777d1209e4b635fc8
rsl140 <rsl140@qq.com>
2020-09-03 15:58:04 +0800

    feat: 16进制转ascii

--- function/public.js
+++ function/public.js
@@ -156,4 +156,24 @@ publicjs.debounce = (func, delay) => {
   }
 }
 
+
+/* 十六进制转ascii */
+publicjs.hex2a = (hexx) => {
+  var hex = hexx.toString()// force conversion
+  var str = ''
+  for (var i = 0; i < hex.length; i += 2) { str += String.fromCharCode(parseInt(hex.substr(i, 2), 16)) }
+  return str
+}
+
+/* ascii转十六进制 */
+publicjs.a2hex = (str) =>{
+  var arr = []
+  for (var i = 0, l = str.length; i < l; i++) {
+    var hex = Number(str.charCodeAt(i)).toString(16)
+    arr.push(hex)
+  }
+  return arr.join('')
+}
+
+
 export default publicjs
