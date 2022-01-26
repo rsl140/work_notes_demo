810a61d42737b50167a44f60ecd8f70fafe0cf56
rsl <raosiling@scmagic.cn>
2022-01-26 19:32:41 +0800

    fix: ipc通信

--- electron-vue2/src/components/HelloWorld.vue
+++ electron-vue2/src/components/HelloWorld.vue
@@ -1,34 +1,19 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="mb10">
+      <el-input class="mr10 w40" v-model="saveStoreName" placeholder="storeName"></el-input>
+      <el-input class="mr10 w40" v-model="saveStoreVal" placeholder="storeVal"></el-input>
+      <el-button class="mr10 w10" @click="saveStore">saveStore</el-button>
+    </div>
+    <div class="mb10">showGetStore:{{showGetStore}}</div>
+    <div class="mb10">
+      <el-input class="mr10 w40" v-model="getStoreName" placeholder="getStoreName"></el-input>
+      <el-input class="mr10 w40" v-model="getStoreCallBackName" placeholder="getStoreCallBackName"></el-input>
+      <el-button class="mr10 w10" @click="getStore">getStore</el-button>
+    </div>
+    <div class="mb10">
+      <el-button class="mr10 w10" @click="about">about</el-button>
+    </div>
   </div>
 </template>
 
@@ -37,6 +22,47 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      saveStoreName: '',
+      saveStoreVal: '',
+      getStoreName: '',
+      getStoreCallBackName: '',
+      showGetStore: ''
+    }
+  },
+  methods: {
+    about () {
+      try {
+        const ipc = window.ipc
+        ipc.send('about')
+      } catch (error) {
+
+      }
+    },
+    saveStore () {
+      try {
+        const ipc = window.ipc
+        if (ipc) {
+          ipc.send('saveStore', { storeName: this.saveStoreName, val: this.saveStoreVal })
+        }
+      } catch (error) {
+      }
+    },
+    getStore () {
+      try {
+        const ipc = window.ipc
+        if (ipc) {
+          ipc.send('getStore', { storeName: this.getStoreName, callBackName: this.getStoreCallBackName })
+          ipc.on(this.getStoreCallBackName, (e, f) => {
+            console.log(f)
+            this.showGetStore = f
+          })
+        }
+      } catch (error) {
+      }
+    }
   }
 }
 </script>
@@ -56,5 +82,21 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.mr10 {
+  margin-right: 10px;
+}
+
+.mb10 {
+  margin-bottom: 10px;
+}
+
+.w40 {
+  width: 40%;
+}
+
+.w10 {
+  width: 10%;
 }
 </style>
--- electron-vue3/src/main.js
+++ electron-vue3/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(ElementPlus, { size: 'small' }).mount('#app')
