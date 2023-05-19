8e692527a94f0b264056fd7a128d335f68a81621
Summer <rsl140@qq.com>
2023-05-19 11:35:13 +0800

    Update download.js

--- function/download.js
+++ function/download.js
@@ -21,3 +21,37 @@
     // 然后移除
     document.body.removeChild(eleLink)
  }
+
+
+
+/**
+ * 下载文件
+ * @param {String} path - 下载地址/下载请求地址。
+ * @param {String} name - 下载文件的名字/重命名（考虑到兼容性问题，最好加上后缀名）
+ */
+export function downloadFile (path, name) {
+  const xhr = new XMLHttpRequest()
+  xhr.open('get', path)
+  xhr.responseType = 'blob'
+  xhr.send()
+  xhr.onload = function () {
+    if (this.status === 200 || this.status === 304) {
+      // 如果是IE10及以上，不支持download属性，采用msSaveOrOpenBlob方法，但是IE10以下也不支持msSaveOrOpenBlob
+      if ('msSaveOrOpenBlob' in navigator) {
+        navigator.msSaveOrOpenBlob(this.response, name)
+        return
+      }
+      // const blob = new Blob([this.response], { type: xhr.getResponseHeader('Content-Type') });
+      // const url = URL.createObjectURL(blob);
+      const url = URL.createObjectURL(this.response)
+      const a = document.createElement('a')
+      a.style.display = 'none'
+      a.href = url
+      a.download = name
+      document.body.appendChild(a)
+      a.click()
+      document.body.removeChild(a)
+      URL.revokeObjectURL(url)
+    }
+  }
+}
